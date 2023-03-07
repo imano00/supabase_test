921ce52624375377c64e0e1ee459f8d0ef67c9f2
redfield <redfieldchristabel@gmail.com>
2023-03-07 16:12:36 +0800

    add

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,16 +1,18 @@
-import { Router, RouterContext } from "https://deno.land/x/oak@v12.1.0/mod.ts";
+import { Router } from "https://deno.land/x/oak@v12.1.0/mod.ts";
 
 const router = new Router();
 
-router.get("/users", (ctx: RouterContext) => {
-  ctx.response.body = "List of users";
+router.get("/", (ctx) => {
+  ctx.response.body = "Hello World!";
 });
 
-router.get("/user/:id", (ctx: RouterContext) => {
+router.get("/user/:id", (ctx) => {
   const { id } = ctx.params;
   ctx.response.body = `User with ID ${id}`;
 });
 
 const userRouter = new Router();
 
-export default router;
+userRouter.prefix("user");
+
+export default userRouter;
